31fc9f39af1c0cab040414d260d3044e674b661e
Kevin Wanjala <wanjalahkevin97@gmail.com>
2024-05-23 13:18:06 +0300

    Initial commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [x for x in num_list if x % 2 == 0]
+    return evens
+
+
 
 def make_exclamation(sentence_list):
-    pass
+    # Use list comprehension to add exclamation marks to each sentence
+    sentences_with_exclamation = [sentence + "!" for sentence in sentence_list]
+    return sentences_with_exclamation
+
+
